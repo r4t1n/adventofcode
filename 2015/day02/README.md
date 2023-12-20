57bf59487acf4cb3e23d3b51df8bc1cd70a78a60
r4t1n <r4t1n@proton.me>
2023-12-20 20:38:46 +0100

    Solve 2015/day02/part2

--- 2015/day02/README.md
+++ 2015/day02/README.md
@@ -2,8 +2,22 @@
 
 ## Part 1
 
-example1.txt should output 58
+example.txt should output 58
 
 The formula for calculating the slack is the product of the two smallest dimension: 
 
 **smallest dimension** * **second smallest dimension**
+
+## Part 2
+
+example.txt should output 34
+
+The ribbon needs to only wrap it around four sides, so use the smallest two dimensions:
+
+**2x3x4**: **2 + 2 + 3 + 3** = **10**
+
+then we need to make the bow which is just the product of all dimensions:
+
+**2x3x4**: **2 * 3 * 4** = **24**
+
+![Square prism](square-prism.svg)
---
+++ 2015/day02/part2.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func extractDimensions(line string, length, width, height *int) {
+	pattern := regexp.MustCompile(`([0-9]+)x([0-9]+)x([0-9]+)`) // Nice static fucking pattern :3
+	matches := pattern.FindAllStringSubmatch(line, 1)
+
+	for _, match := range matches {
+		*length, _ = strconv.Atoi(match[1])
+		*width, _ = strconv.Atoi(match[2])
+		*height, _ = strconv.Atoi(match[3])
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: go run %s.go <input>\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	input, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(1)
+	}
+
+	var length, width, height int
+	var ribbonSum int
+	for _, line := range strings.Split(string(input), "\n") { // Iterate over each line from input
+		if len(line) > 0 { // Check the line is not whitespace
+			extractDimensions(string(line), &length, &width, &height)               // Extract the dimensions from the line
+			dimensions := []int{length, width, height}                              // Put the dimensions in a slice
+			sort.Ints(dimensions)                                                   // Sort the dimensions in ascending order
+			ribbon := dimensions[0] + dimensions[0] + dimensions[1] + dimensions[1] // Calculate the ribbon size (sum of two smallest dimensions twice)
+			bow := dimensions[0] * dimensions[1] * dimensions[2]                    // Calculate the bow size (product of all dimensions)
+			totalRibbon := ribbon + bow                                             // Calculate the total area
+			ribbonSum += totalRibbon                                                // Add the total area for the line to the totalAreas variable
+			fmt.Printf("Line: %s, length: %d, width: %d, height: %d, ribbon: %d, bow: %d, total ribbon: %d\n", line, length, width, height, ribbon, bow, totalRibbon)
+		}
+	}
+
+	fmt.Println("\nSum:", ribbonSum)
+}
