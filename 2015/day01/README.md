ccefb737065062ae9a89532ca2840ceece1d3fe0
r4t1n <r4t1n@proton.me>
2023-12-20 17:41:11 +0100

    Solve 2015/day01 part 2

--- 2015/day01/README.md
+++ 2015/day01/README.md
@@ -3,3 +3,7 @@
 ## Part 1
 
 example1.txt should output 0
+
+## Part 2
+
+example2.txt should output 5
---
+++ 2015/day01/part2.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"unicode"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run part1.go <input>")
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	input, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(1)
+	}
+
+	var floor int
+	var position int
+	var basementPosition int
+	for _, char := range input {
+		if !unicode.IsSpace(rune(char)) { // Don't iterate over a new line
+			if char == '(' {
+				floor++
+				position++
+				fmt.Println("Up")
+			} else if char == ')' {
+				floor--
+				position++
+				fmt.Println("Down")
+			} else {
+				fmt.Printf("Character: %c is invalid (not a bracket)\n", char)
+			}
+
+			if floor == -1 && basementPosition == 0 {
+				basementPosition = position
+				fmt.Println("First at basement")
+			}
+		}
+	}
+
+	fmt.Printf("\nFirst basement position: %d\n", basementPosition)
+}
