8fc15c8a8b7bc3eb823e11b9f1ba73c2f5506fc4
r4t1n <r4t1n@proton.me>
2024-01-09 13:13:56 +0100

    Solve 2015/04 part 1

---
+++ 2015/04/part1/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func puzzle(input []byte) {
+	prefix := "00000"
+	var number int
+	for {
+		checksum := md5.Sum([]byte(fmt.Sprintf("%s%d", input, number)))
+		if strings.HasPrefix(fmt.Sprintf("%x", checksum), prefix) {
+			break
+		}
+		number++
+	}
+
+	fmt.Printf("First number with hash prefix %s: %d\n", prefix, number)
+}
+
+func main() {
+	if len(os.Args) < 2 { // Check if input is provided
+		fmt.Println("Please provide the input as the first argument (go run main.go <input>)")
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]              // Get the input file from the first argument
+	input, err := os.ReadFile(inputFile) // Read the input from the input file
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(1)
+	}
+
+	puzzle(input)
+}
