0ff53a396cde510aca202baaa58879ff5abbe5d5
r4t1n <r4t1n@proton.me>
2024-02-11 03:35:43 +0100

    Add example.rs

--- example.rs
+++ example.rs
@@ -1,3 +1,24 @@
+use std::env::args;
+use std::fs;
+use std::process;
+
+fn puzzle(input: &str) {
+    println!("{input}");
+}
+
 fn main() {
-    println!("Hello, World!");
+    let args: Vec<String> = args().collect();
+    if args.len() != 2 {
+        println!("Pass the input file as the second argument");
+        process::exit(1);
+    }
+    let input_filename = &args[1];
+    let input = match fs::read_to_string(input_filename) {
+        Ok(content) => content,
+        Err(err) => {
+            eprintln!("Error reading file: {err}");
+            process::exit(1);
+        }
+    };
+    puzzle(&input);
 }
