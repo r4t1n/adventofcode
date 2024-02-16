f1014b7017b4e204d2b0e56f00b14ef0d45baef3
r4t1n <r4t1n@proton.me>
2024-02-16 05:03:23 +0100

    Optimize 2015/06 by using an array instead of HASHMAPS!!!

--- 2015/06/src/main.rs
+++ 2015/06/src/main.rs
@@ -1,11 +1,10 @@
-use std::collections::HashMap;
 use std::env::args;
 use std::fs;
 use std::process;
 
 fn puzzle(input: &str) -> (u32, u32) {
-    let mut grid_part_1: HashMap<(u16, u16), bool> = HashMap::new();
-    let mut grid_part_2: HashMap<(u16, u16), u32> = HashMap::new();
+    let mut grid_part_1: [[bool; 1000]; 1000] = [[false; 1000]; 1000];
+    let mut grid_part_2: [[u32; 1000]; 1000] = [[0; 1000]; 1000];
     let mut lit_lights: u32 = 0;
     let mut total_brightness: u32 = 0;
 
@@ -17,12 +16,13 @@ fn puzzle(input: &str) -> (u32, u32) {
         } else if line.starts_with("turn off") {
             "turn off"
         } else {
+            println!("[!] Invalid line: {}", line);
             continue;
         };
 
         let coords: Vec<u16> = line
             .split(|c: char| !c.is_numeric())
-            .filter_map(|s| s.parse().ok())
+            .filter_map(|s: &str| s.parse().ok())
             .collect();
 
         if coords.len() != 4 {
@@ -30,41 +30,40 @@ fn puzzle(input: &str) -> (u32, u32) {
             continue;
         }
 
-        modify_grid_part_1(&mut grid_part_1, instruction, coords.clone());
-        modify_grid_part_2(&mut grid_part_2, instruction, coords);
+        modify_grid(&mut grid_part_1, &mut grid_part_2, instruction, coords);
     }
 
-    for &state in grid_part_1.values() {
-        if state {
-            lit_lights += 1;
+    for row in grid_part_1.iter() {
+        for &state in row.iter() {
+            if state {
+                lit_lights += 1;
+            }
         }
     }
 
-    for &brightness in grid_part_2.values() {
-        total_brightness += brightness;
+    for row in grid_part_2.iter() {
+        for &brightness in row.iter() {
+            total_brightness += brightness;
+        }
     }
 
     (lit_lights, total_brightness)
 }
 
-fn modify_grid_part_1(grid: &mut HashMap<(u16, u16), bool>, instruction: &str, coords: Vec<u16>) {
-    let operation: fn(bool) -> bool = match instruction {
+fn modify_grid(
+    grid_part_1: &mut [[bool; 1000]; 1000],
+    grid_part_2: &mut [[u32; 1000]; 1000],
+    instruction: &str,
+    coords: Vec<u16>,
+) {
+    let instruction_part_1: fn(bool) -> bool = match instruction {
         "turn on" => |_| true,
         "turn off" => |_| false,
         "toggle" => |state: bool| !state,
         _ => return,
     };
 
-    for x in coords[0]..=coords[2] {
-        for y in coords[1]..=coords[3] {
-            let entry: &mut bool = grid.entry((x, y)).or_insert(false);
-            *entry = operation(*entry);
-        }
-    }
-}
-
-fn modify_grid_part_2(grid: &mut HashMap<(u16, u16), u32>, instruction: &str, coords: Vec<u16>) {
-    let operation: fn(u32) -> u32 = match instruction {
+    let instruction_part_2: fn(u32) -> u32 = match instruction {
         "turn on" => |brightness: u32| brightness + 1,
         "turn off" => |brightness: u32| brightness.saturating_sub(1),
         "toggle" => |brightness: u32| brightness + 2,
@@ -73,8 +72,10 @@ fn modify_grid_part_2(grid: &mut HashMap<(u16, u16), u32>, instruction: &str, co
 
     for x in coords[0]..=coords[2] {
         for y in coords[1]..=coords[3] {
-            let entry: &mut u32 = grid.entry((x, y)).or_insert(0);
-            *entry = operation(*entry);
+            grid_part_1[x as usize][y as usize] =
+                instruction_part_1(grid_part_1[x as usize][y as usize]);
+            grid_part_2[x as usize][y as usize] =
+                instruction_part_2(grid_part_2[x as usize][y as usize]);
         }
     }
 }
@@ -82,7 +83,7 @@ fn modify_grid_part_2(grid: &mut HashMap<(u16, u16), u32>, instruction: &str, co
 fn main() {
     let args: Vec<String> = args().collect();
     if args.len() != 2 {
-        println!("Pass the input file as the second argument");
+        println!("[!] Usage: {} <input file>", args[0]);
         process::exit(1);
     }
 
@@ -90,7 +91,7 @@ fn main() {
     let input: String = match fs::read_to_string(input_filename) {
         Ok(content) => content.trim().to_string(),
         Err(err) => {
-            eprintln!("Error reading file: {}", err);
+            eprintln!("[!] Error reading file: {}", err);
             process::exit(1);
         }
     };
