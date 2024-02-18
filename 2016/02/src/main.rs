945d47653996598cba94e7f4083262642b6a6ab9
r4t1n <r4t1n@proton.me>
2024-02-18 01:09:35 +0100

    Solve 2016/02

--- 2016/02/src/main.rs
+++ 2016/02/src/main.rs
@@ -2,35 +2,91 @@ use std::env::args;
 use std::fs;
 use std::process;
 
-fn puzzle(input: &str) -> (String, i32) {
-    let answer_part_2: i32 = 0;
-    let mut code: String = String::new();
-    let mut values_x: Vec<i8> = Vec::new();
-    let mut values_y: Vec<i8> = Vec::new();
-    let mut x: i8 = 0;
-    let mut y: i8 = 0;
+fn puzzle(input: &str) -> (String, String) {
+    let mut code_part_1: String = String::new();
+    let mut code_part_2: String = String::new();
+    let mut x_part_1: i8 = 0;
+    let mut x_part_2: i8 = -2;
+    let mut x_values_part_1: Vec<i8> = Vec::new();
+    let mut x_values_part_2: Vec<i8> = Vec::new();
+    let mut y_part_1: i8 = 0;
+    let mut y_part_2: i8 = 0;
+    let mut y_values_part_1: Vec<i8> = Vec::new();
+    let mut y_values_part_2: Vec<i8> = Vec::new();
 
     for line in input.lines() {
         for char in line.chars() {
             match char {
                 'U' => {
-                    if y < 1 {
-                        y += 1;
+                    if y_part_1 < 1 {
+                        y_part_1 += 1;
+                    }
+                    match x_part_2.abs() {
+                        0 => {
+                            if y_part_2 < 2 {
+                                y_part_2 += 1;
+                            }
+                        }
+                        1 => {
+                            if y_part_2 < 1 {
+                                y_part_2 += 1;
+                            }
+                        }
+                        _ => {}
                     }
                 }
                 'D' => {
-                    if y > -1 {
-                        y -= 1;
+                    if y_part_1 > -1 {
+                        y_part_1 -= 1;
+                    }
+                    match x_part_2.abs() {
+                        0 => {
+                            if y_part_2 > -2 {
+                                y_part_2 -= 1;
+                            }
+                        }
+                        1 => {
+                            if y_part_2 > -1 {
+                                y_part_2 -= 1;
+                            }
+                        }
+                        _ => {}
                     }
                 }
                 'L' => {
-                    if x > -1 {
-                        x -= 1;
+                    if x_part_1 > -1 {
+                        x_part_1 -= 1;
+                    }
+                    match y_part_2.abs() {
+                        0 => {
+                            if x_part_2 > -2 {
+                                x_part_2 -= 1;
+                            }
+                        }
+                        1 => {
+                            if x_part_2 > -1 {
+                                x_part_2 -= 1;
+                            }
+                        }
+                        _ => {}
                     }
                 }
                 'R' => {
-                    if x < 1 {
-                        x += 1;
+                    if x_part_1 < 1 {
+                        x_part_1 += 1;
+                    }
+                    match y_part_2.abs() {
+                        0 => {
+                            if x_part_2 < 2 {
+                                x_part_2 += 1;
+                            }
+                        }
+                        1 => {
+                            if x_part_2 < 1 {
+                                x_part_2 += 1;
+                            }
+                        }
+                        _ => {}
                     }
                 }
                 _ => {
@@ -40,22 +96,32 @@ fn puzzle(input: &str) -> (String, i32) {
             }
         }
 
-        values_x.push(x);
-        values_y.push(y);
+        x_values_part_1.push(x_part_1);
+        x_values_part_2.push(x_part_2);
+        y_values_part_1.push(y_part_1);
+        y_values_part_2.push(y_part_2);
     }
 
-    for (x, y) in values_x.iter().zip(&values_y) {
-        if let Some(key) = get_key(*x, *y) {
-            code.push(key);
+    for (x, y) in x_values_part_1.iter().zip(&y_values_part_1) {
+        if let Some(key) = get_key_part_1(*x, *y) {
+            code_part_1.push(key);
         } else {
             println!("[!] Invalid coordinate: {}, {}", x, y);
         }
     }
 
-    (code, answer_part_2)
+    for (x, y) in x_values_part_2.iter().zip(&y_values_part_2) {
+        if let Some(key) = get_key_part_2(*x, *y) {
+            code_part_2.push(key);
+        } else {
+            println!("[!] Invalid coordinate: {}, {}", x, y);
+        }
+    }
+
+    (code_part_1, code_part_2)
 }
 
-fn get_key(x: i8, y: i8) -> Option<char> {
+fn get_key_part_1(x: i8, y: i8) -> Option<char> {
     let keypad: [[char; 3]; 3] = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']];
 
     let index_row: usize = match y {
@@ -69,6 +135,36 @@ fn get_key(x: i8, y: i8) -> Option<char> {
         -1 => 0,
         0 => 1,
         1 => 2,
+        _ => return None,
+    };
+
+    Some(keypad[index_row][index_col])
+}
+
+fn get_key_part_2(x: i8, y: i8) -> Option<char> {
+    let keypad: [[char; 5]; 5] = [
+        ['0', '0', '1', '0', '0'],
+        ['0', '2', '3', '4', '0'],
+        ['5', '6', '7', '8', '9'],
+        ['0', 'A', 'B', 'C', '0'],
+        ['0', '0', 'D', '0', '0'],
+    ];
+
+    let index_row: usize = match y {
+        -2 => 4,
+        -1 => 3,
+        0 => 2,
+        1 => 1,
+        2 => 0,
+        _ => return None,
+    };
+
+    let index_col: usize = match x {
+        -2 => 0,
+        -1 => 1,
+        0 => 2,
+        1 => 3,
+        2 => 4,
         _ => return None,
     };
 
--- README.md
+++ README.md
@@ -8,15 +8,16 @@ Solving Advent of Code in Rust
 
 |Day|Part 1|Part 2|
 |:-:|:----:|:----:|
-|[Day 1](2016/01)|⭐|⭐|
+|[Day 1](2016/01/src/main.rs)|⭐|⭐|
+|[Day 2](2016/02/src/main.rs)|⭐|⭐|
 
 ### 2015
 
 |Day|Part 1|Part 2|
 |:-:|:----:|:----:|
-|[Day 1](2015/01)|⭐|⭐|
-|[Day 2](2015/02)|⭐|⭐|
-|[Day 3](2015/03)|⭐|⭐|
-|[Day 4](2015/04)|⭐|⭐|
-|[Day 5](2015/05)|⭐|⭐|
-|[Day 6](2015/06)|⭐|⭐|
+|[Day 1](2015/01/src/main.rs)|⭐|⭐|
+|[Day 2](2015/02/src/main.rs)|⭐|⭐|
+|[Day 3](2015/03/src/main.rs)|⭐|⭐|
+|[Day 4](2015/04/src/main.rs)|⭐|⭐|
+|[Day 5](2015/05/src/main.rs)|⭐|⭐|
+|[Day 6](2015/06/src/main.rs)|⭐|⭐|
